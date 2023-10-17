2fd5d3c754671d5402c69dfde7cd53c34cebc633
emissaodev <emissaodev@gmail.com>
2023-10-17 04:21:48 -0300

    criando validação usuario

--- SistemaLogin/CadastroUsuarios.cs
+++ SistemaLogin/CadastroUsuarios.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace SistemaLogin
 {
-    internal class CadastroUsuarios
+    static class CadastroUsuarios
     {
+        private static Usuario[] usuarios = 
+        { 
+            new Usuario()  {Nome = "Emerson",Senha ="abc123"},
+            new Usuario() {Nome ="Danny", Senha = "123abc"},
+            new Usuario() {Nome ="Bruno", Senha = "abcd12345"},
+        };
+        private static Usuario _userLogado = null; 
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; }
+            private set { _userLogado = value; }
+        }
+        public static bool Login(string nome, string senha)
+        {
+            foreach(Usuario usuario in usuarios) 
+            {
+                if(usuario.Nome == nome && usuario.Senha == senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
